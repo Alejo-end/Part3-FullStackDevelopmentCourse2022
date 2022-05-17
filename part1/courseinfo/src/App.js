62a2158428b90485b0c17ad03ca2d6cc087edfad
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-05-17 17:18:09 +0300

    adding unicafe app

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,32 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
 
 const App = () => {
   const course = {
@@ -18,41 +47,12 @@ const App = () => {
     ]
   }
 
-  const Header = () => {
-    return (
-      <h1>{course}</h1>
-    )
-  }
-  const Part = (part, exercise) => {
-    <p>
-      {part} {exercise}
-    </p>
-  }
-  const Content = (parts) => {
-
-    return (
-      <>
-        <Part part={parts[0].name} exercise={parts[0].exercises1} />
-        <Part part={parts[1].name} exercise={parts[1].exercises2} />
-        <Part part={parts[2].name} exercise={parts[2].exercises3} />
-      </>
-    )
-  }
-
-
-  const Total = () => {
-    return (
-      <p>Number of exercises {course.parts[0].exercises1 + course.parts[1].exercises2 + course.parts[2].exercises3}</p>
-    )
-  }
-
-
   return (
-    <div>
-      <Header />
-      <Content parts={course} />
-      <Total />
-    </div>
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
   )
 }
 
