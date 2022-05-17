f54619beab84bef78236564bcba8507ad6ff692d
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-05-17 17:37:03 +0300

    part1.10 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,37 @@
 import { useState } from 'react'
-import  Statistics  from './Statistics'
+import Statistics from './Statistics'
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = good / all * 100;
+
 
   return (
     <div>
       <h1>Unicafe</h1>
       <h2>Give Feedback</h2>
       <div className='buttons'>
-        <button onClick={() => setGood(good + 1)}>Good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-        <button onClick={() => setBad(bad + 1)}>Bad</button>
-      </div>
-      <h2>Statistics</h2>
-      <Statistics label={"good"} count={good} />
-      <Statistics label={"neutral"} count={neutral} />
-      <Statistics label={"bad"} count={bad} />
+            <button onClick={() => setGood(good + 1)}>Good</button>
+            <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+            <button onClick={() => setBad(bad + 1)}>Bad</button>
+          </div>
+
+          <h2>Statistics</h2>
+      {all > 0 ? (
+        <>
+
+          <Statistics label={"good"} count={good} />
+          <Statistics label={"neutral"} count={neutral} />
+          <Statistics label={"bad"} count={bad} />
+          <Statistics label={"all"} count={bad} />
+          <Statistics label={"average"} count={average} />
+          <Statistics label={"positive"} count={positive} /></>) : <p>No feedback given</p>
+      }
 
     </div>
   )
--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -3,8 +3,8 @@
 const Statistics = ({ label, count }) => {
 
     return (
-        <div style={{ display: "flex", alignItems: "center"}}>
-            <h3>{label+ ": "} </h3>
+        <div style={{ display: "flex", alignItems: "center", padding: 0}}>
+            <h4>{label+ ": "} </h4>
             <p>{count}</p>
         </div>
     )
