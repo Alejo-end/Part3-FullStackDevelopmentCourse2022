9c2b11976c89ebf980423f151a48950dd042a7a2
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-05-18 20:16:44 +0300

    assignment 1.14 anecdotes.

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,4 +1,44 @@
-import { useState } from 'react';
+import { useState } from 'react'
+
+const Button = ({ clickFunc, description }) => (  //similar to as shown in the tutorial about rendering in part 1
+  <button onClick={clickFunc}>
+    {description}
+    </button>
+)
+
+const Header = ({header}) => {
+  return (
+    <div>
+      <h1>{header}</h1>
+    </div>
+  )
+}
+
+const DisplayVotes = ({votes, selected}) => {
+  return (
+    <p>Total votes: {votes[selected]}</p>
+  )
+}
+
+const DisplayAnecdote = ({anecdotes, selected}) => {
+  return (
+    <p>{anecdotes[selected]}</p>
+  )
+}
+
+const AnecMostVotes = ({votes, anecdotes}) => {
+  const header = 'Anecdote with most votes'
+  const highestVotes = Math.max(...votes) //find highest vote count in votes array
+  const anecIndex = votes.indexOf(highestVotes) //find corresponding index of anecdote
+
+  return (
+    <div>
+      <Header header={header}/>
+      <DisplayAnecdote anecdotes={anecdotes} selected={anecIndex}/>
+      <DisplayVotes votes={votes} selected={anecIndex}/>
+    </div>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -12,17 +52,34 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(Array(anecdotes.length).fill(0))
 
-  // setSelected to random anecdote
-  const handleAnecdote = () => {
-    let n = 0
-    setSelected(n)
+  const nextAnec = 'Next anecdote'
+  const voteText = 'Upvote'
+  const anecHeader = 'Anecdote of the day'
+
+  const selectRandomAnecdote = () => {
+    let nextAnecdoteNum = Math.floor(Math.random() * anecdotes.length)
+    while(nextAnecdoteNum === selected) { //to prevent the same anecdote from being selected twice in a row
+      nextAnecdoteNum = Math.floor(Math.random() * anecdotes.length)
+    }
+    setSelected(nextAnecdoteNum)
+  }
+
+  const upvote = () => {
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVote(votesCopy)
   }
 
   return (
     <div>
-      {anecdotes[selected]}
-      <button onClick={handleAnecdote}>Next Anecdote</button>
+      <Header header={anecHeader}/>
+      <DisplayAnecdote anecdotes={anecdotes} selected={selected}/>
+      <DisplayVotes votes={votes} selected={selected}/>
+      <Button clickFunc={selectRandomAnecdote} description={nextAnec}/>
+      <Button clickFunc={upvote} description={voteText}/>
+      <AnecMostVotes votes={votes} anecdotes={anecdotes}/>
     </div>
   )
 }
