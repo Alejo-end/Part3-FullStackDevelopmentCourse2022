9d108afbd67aebb1750379c58ae8527fdb62f2ab
Alejo-end <alejandro.deleon@utp.ac.pa>
2022-07-17 13:44:54 +0300

    part3.9 with backend connection

--- part2/phonebook/src/services/server.js
+++ part2/phonebook/src/services/server.js
@@ -1,27 +1,27 @@
 import axios from 'axios'
-const url = 'http://localhost:3001/persons'
+const url = 'http://localhost:3001'
 
 //as instructed in 2.16, similar as what was shown in the example. Due to a react warning I assigned the default exports to a variable first.
 const getAllPersons = async () => {
-    const request = axios.get(url)
+    const request = axios.get(url + '/api/persons')
     const response = await request
     return response.data
 }
 
 const createPerson = async newPerson => {
-    const request = axios.post(url, newPerson)
+    const request = axios.post(url + '/api/persons', newPerson)
     const response = await request
     return response.data
 }
 
 const updatePerson = async (id, newPerson) => {
-    const request = axios.put(`${url}/${id}`, newPerson)
+    const request = axios.put(`${url} + '/api/persons/' + ${id}`, newPerson)
     const response = await request
     return response.data
 }
 
 const deletePerson = async (id) => {
-    const request = axios.delete(`${url}/${id}`)
+    const request = axios.delete(`${url} + '/api/persons/' + ${id}`)
     const response = await request
     return response.data
 }
